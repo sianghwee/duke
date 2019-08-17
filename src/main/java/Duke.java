a829a44cd7b8eb604b5771099836244b41c97ba9
Siang Hwee <siang_hwee@hotmail.com>
2019-08-17 14:04:26 +0800

    Add support for tracking ToDos, Deadlines and Events task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    private static ArrayList<Task> db = new ArrayList<>();
+
     public static void printLogo() {
         String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
@@ -14,7 +16,7 @@ public class Duke {
         System.out.println("\t" + line);
     }
 
-    public static void printList(ArrayList<Task> db) {
+    public static void printList() {
         System.out.println("\t Here are the tasks in your list:");
         int index = 1;
         for (Task task : db) {
@@ -31,10 +33,16 @@ public class Duke {
         System.out.println("\t  " + task.toString());
     }
 
+    public static void updateList(Task task) {
+        db.add(task);
+        System.out.println("\t Got it. I've added this task:");
+        System.out.println("\t  " + task);
+        System.out.println(String.format("\t Now you have %d tasks in your list", db.size()));
+    }
+
     public static void main(String[] args) {
         printLogo();
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> db = new ArrayList<>();
 
         printLine();
         System.out.println("\t Hello! I'm Duke");
@@ -43,21 +51,33 @@ public class Duke {
         System.out.println();
 
         String command;
+        String description;
+        String[] split;
         while (!(command = sc.next()).equals("bye")) {
             printLine();
             switch (command) {
             case "list":
-                printList(db);
+                printList();
                 break;
             case "done":
                 int index = sc.nextInt();
                 Task selectedTask = db.get(index - 1);
                 updateTask(selectedTask);
                 break;
-            default:
-                command = command + sc.nextLine();
-                db.add(new Task(command));
-                System.out.println("\t added: " + command);
+            case "todo":
+                description = sc.nextLine().trim();
+                updateList(new Todo(description));
+                break;
+            case "deadline":
+                description = sc.nextLine().trim();
+                split = description.split("/");
+                updateList(new Deadline(split[0], split[1]));
+                break;
+            case "event":
+                description = sc.nextLine().trim();
+                split = description.split("/");
+                updateList(new Event(split[0], split[1]));
+                break;
             }
             printLine();
             System.out.println();
