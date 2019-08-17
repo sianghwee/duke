88cd8f5daa30108fb75db468f755a9d32336b81d
Siang Hwee <siang_hwee@hotmail.com>
2019-08-17 17:37:02 +0800

    Added a parent class for all exceptions related to Duke and children classes for individual errors

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,11 @@
+public class DukeException extends Exception {
+}
+
+class InvalidDescription extends DukeException {
+}
+
+class InsufficientArgument extends DukeException {
+}
+
+class InvalidCommand extends DukeException {
+}
