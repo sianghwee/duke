e2e279e57e9e3d7e0ac8c5aebd8ca601018d75f7
Siang Hwee <siang_hwee@hotmail.com>
2019-08-27 00:54:32 +0800

    Created a class for all the user interfaces

---
+++ src/main/java/Ui.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Ui {
+    public void printLine() {
+        String line = "____________________________________________________________";
+        System.out.println("\t" + line);
+    }
+
+    public void welcome() {
+        this.printLine();
+        System.out.println("\t Hello, I'm Sapu!");
+        System.out.println("\t What can I do for you?");
+        this.printLine();
+    }
+
+    public void printList(TaskList db) {
+        System.out.println("\t Here are the tasks in your list:");
+        int index = 1;
+        for (Task task : db.getList()) {
+            String output = String.format("\t %d. %s", index, task.toString());
+            System.out.println(output);
+            index++;
+        }
+    }
+
+    public String readCommand() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    public void deleteMessage(Task task, TaskList db) {
+        System.out.println("\t Noted. I've removed this task:");
+        System.out.println("\t  " + task);
+        System.out.println(String.format("\t Now you have %d tasks in your list", db.size()));
+    }
+
+    public void updateMessage(Task task) {
+        System.out.print("\t ");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("\t  " + task.toString());
+    }
+
+    public void byeMessage() {
+        printLine();
+        System.out.println("\t Bye. Hope to see you again soon!");
+        printLine();
+    }
+}
