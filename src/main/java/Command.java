74079641a91fdbb9b4b7993df4854569eab63367
Siang Hwee <siang_hwee@hotmail.com>
2019-08-27 23:52:02 +0800

    Removed the TestCommand

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,9 +1,3 @@
-interface Command {
+public interface Command {
     public void execute(TaskList tasks, Ui ui, Storage storage);
-}
-
-class TestCommand implements Command {
-    public void execute(TaskList tasks, Ui ui, Storage storage) {
-        System.out.println("executed");
-    }
 }
