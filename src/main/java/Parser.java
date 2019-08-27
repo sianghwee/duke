2d4f02edf1626d28fdb454c02c549c1b4bced92c
Siang Hwee <siang_hwee@hotmail.com>
2019-08-27 23:51:18 +0800

    Editted the program to be able to handle exception from incorrect user input

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,18 +1,74 @@
-class Parser {
+public class Parser {
     public static Command parse(String fullCommand) {
-        String[] split = fullCommand.split(" |/");
-        // for (String s : split) {
-        // System.out.print(s);
-        // }
+        String[] split = fullCommand.split(" ", 2);
         switch (split[0]) {
         case "list":
             return new ListCommand();
         case "delete":
-            return new DeleteCommand(Integer.parseInt(split[1]));
+            try {
+                return new DeleteCommand(Integer.parseInt(split[1]));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return new ErrorCommand(new InsufficientArgument());
+            }
         case "done":
-            return new DoneCommand(Integer.parseInt(split[1]));
+            try {
+                return new DoneCommand(Integer.parseInt(split[1]));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return new ErrorCommand(new InsufficientArgument());
+            }
+        case "todo":
+            try {
+                verifyDescription(split, 2);
+                return new AddCommand(new Todo(split[1]));
+            } catch (InvalidDescription e) {
+                return new ErrorCommand(e);
+            }
+        case "event":
+            try {
+                verifyDescription(split, 2);
+                String[] desc = splitDescription(split[1]);
+                return new AddCommand(new Event(desc[0], desc[1]));
+            } catch (InvalidDescription e) {
+                return new ErrorCommand(e);
+            } catch (InsufficientArgument e) {
+                return new ErrorCommand(e);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return new ErrorCommand(new MissingPreposition(split[0]));
+            }
+        case "deadline":
+            try {
+                verifyDescription(split, 2);
+                String[] desc = splitDescription(split[1]);
+                return new AddCommand(new Deadline(desc[0], desc[1]));
+            } catch (InvalidDescription e) {
+                return new ErrorCommand(e);
+            } catch (InsufficientArgument e) {
+                return new ErrorCommand(e);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return new ErrorCommand(new MissingPreposition(split[0]));
+            }
         default:
-            return new TestCommand();
+            return new ErrorCommand(new InvalidCommand());
+        }
+    }
+
+    private static String[] splitDescription(String description) throws InsufficientArgument {
+        String[] split = description.split("/", 2);
+        if (split.length < 2) {
+            throw new InsufficientArgument();
+        }
+        if (split[1].split(" |/").length == 5) {
+            String[] temp = split[1].split(" ", 2);
+            DukeDate dd = new DukeDate(temp[1]);
+            temp[1] = dd.toString();
+            split[1] = String.join(" ", temp);
+        }
+        return split;
+    }
+
+    private static void verifyDescription(String[] description, int size) throws InvalidDescription {
+        if (description.length < size) {
+            throw new InvalidDescription(description[0]);
         }
     }
 }
