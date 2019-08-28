bac8592ad95cf1352063125cbc60f200ce225427
Siang Hwee <siang_hwee@hotmail.com>
2019-08-28 23:00:05 +0800

    Generated the javadoc files for all the java files

---
+++ src/main/java/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"duke.command"},{"l":"duke.date"},{"l":"duke.exception"},{"l":"duke.parser"},{"l":"duke.storage"},{"l":"duke.task"},{"l":"duke.ui"}]
