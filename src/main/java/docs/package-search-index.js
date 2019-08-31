17b1db0ff32ea6a9c78532dce1b1dbf268a9eefc
Siang Hwee <siang_hwee@hotmail.com>
2019-08-31 14:10:33 +0800

    Re-generate all the javadocs for all the classes

--- src/main/java/docs/package-search-index.js
+++ src/main/java/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"duke.command"},{"l":"duke.date"},{"l":"duke.exception"},{"l":"duke.parser"},{"l":"duke.storage"},{"l":"duke.task"},{"l":"duke.ui"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"duke.command"},{"l":"duke.date"},{"l":"duke.driver"},{"l":"duke.exception"},{"l":"duke.parser"},{"l":"duke.storage"},{"l":"duke.task"},{"l":"duke.ui"}]
