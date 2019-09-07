671ba3738a649439ca1b4d6342cd373966a42feb
Siang Hwee <siang_hwee@hotmail.com>
2019-09-07 19:28:16 +0800

    Edit file to work with new Launcher class file

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,11 +1,13 @@
+package duke.driver;
+
 import javafx.application.Application;
-import duke.driver.Duke;
+import duke.ui.Main;
 
 /**
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
